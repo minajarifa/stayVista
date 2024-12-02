83956d200bf22532864b31e80497f2830a901f1c
Your Name <minajmiha02@gmail.com>
2024-12-02 14:41:15 +0600

    added categoris

--- client/src/components/Home/Rooms.jsx
+++ client/src/components/Home/Rooms.jsx
@@ -1,33 +1,34 @@
-
 import Card from './Card'
 import Container from '../Shared/Container'
 import Heading from '../Shared/Heading'
 import LoadingSpinner from '../Shared/LoadingSpinner'
 import { useQuery } from '@tanstack/react-query'
-import useAxiosSecure from '../../hooks/useAxiosSecure'
+import { useSearchParams } from 'react-router-dom'
+import useAxiosCommon from '../../hooks/useAxiosCommon'
 
 const Rooms = () => {
-  const axiosSecure = useAxiosSecure()
+  const axiosCommon = useAxiosCommon()
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+  console.log(category)
+
   
-console.log("axiosSecure",axiosSecure)
-  const {data=[],isLoading} = useQuery({
-    queryKey: ["rooms"],
+  const { data:rooms = [], isLoading } = useQuery({
+    queryKey: ["rooms",category],
     queryFn: async () => {
-      const { data } = await axiosSecure.get("/rooms");
+      const { data } = await axiosCommon.get(`/rooms?category=${category}`);
+      console.log(data)
       return data;
     }
   })
-  console.log(data)
-  
-  
 
   if (isLoading) return <LoadingSpinner />
 
   return (
     <Container>
-      {data && data.length > 0 ? (
+      {rooms && rooms.length > 0 ? (
         <div className='grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
-          {data.map(room => (
+          {rooms.map(room => (
             <Card key={room._id} room={room} />
           ))}
         </div>
